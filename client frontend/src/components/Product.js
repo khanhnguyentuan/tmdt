4731243e1e23dc722cb131c6e2d9c46d3765ebd2
Bao <bao.tranbao12122002@hcmut.edu.vn>
2024-05-09 23:04:14 +0700

    push

--- client frontend/src/components/Product.js
+++ client frontend/src/components/Product.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {data} from '../data'
 import '../css/product.css'
-export default function Product() {
+export default function Product(){
     return (
         <>
             <section className="product">
@@ -10,16 +10,11 @@ export default function Product() {
                         {data.map((course, index) => {
                             return (
                                 <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 product-item" key={index}>
-                                    <div className="card" style={{ width: "18rem" }}>
-                                        <img src="..." className="card-img-top" alt="..." />
+                                    <div className="card">
+                                        <img src={course.imageUrl} className="card-img-top" alt="..." />
                                         <div className="card-body">
-                                            <h5 className="card-title">Card title</h5>
-                                            <p className="card-text">
-                                                Some quick example text to build on the card title and make up the bulk of the card's content.
-                                            </p>
-                                            <a href="#" className="btn btn-primary">
-                                                Go somewhere
-                                            </a>
+                                            <h5 className="card-title">{course.courseName}</h5>
+                                            <p className="card-text">{course.metaDescripton}</p>
                                         </div>
                                     </div>
                                 </div>
---
+++ client frontend/src/data.js
@@ -0,0 +1,30 @@
+export const data = [
+    {
+        categoryName: 'Java',
+        courseName: 'àkhaklshkla',
+        metaDescription: 'áklfhkashfklsa',
+        imageUrl: require('./images/img1.jpg')
+    },
+
+    {
+        categoryName: 'Java',
+        courseName: 'Gia Bao',
+        metaDescription: 'fajshfkashklf',
+        imageUrl: require('./images/img2.jpg')
+    },
+
+    {
+        categoryName: 'Java',
+        courseName: 'Món ăn ba miền',
+        metaDescription: 'dhfklhskldhkls',
+        imageUrl: require('./images/img3.jpg')
+    },
+
+    {
+        categoryName: 'Java',
+        courseName: 'hfkashkfhask',
+        metaDescription: 'skfklaskfhaskl',
+        imageUrl: require('./images/img4.jpg')
+    },
+
+]
