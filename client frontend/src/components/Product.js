5960fe8ff51598010daea20ca7cb3f4b48ff0b36
Bao <bao.tranbao12122002@hcmut.edu.vn>
2024-05-09 22:49:14 +0700

    push

--- client frontend/src/components/Product.js
+++ client frontend/src/components/Product.js
@@ -10,11 +10,16 @@ export default function Product() {
                         {data.map((course, index) => {
                             return (
                                 <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 product-item" key={index}>
-                                    <div className="card">
-                                        <img src={course.imageUrl} className="card-img-top" alt="..." />
+                                    <div className="card" style={{ width: "18rem" }}>
+                                        <img src="..." className="card-img-top" alt="..." />
                                         <div className="card-body">
-                                            <h5 className="card-title">{course.courseName}</h5>
-                                            <p className="card-text">{course.metaDescripton}</p>
+                                            <h5 className="card-title">Card title</h5>
+                                            <p className="card-text">
+                                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                                            </p>
+                                            <a href="#" className="btn btn-primary">
+                                                Go somewhere
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
